32cf2383a01bc8cef44e4706d62a14a2ab0dbcd4
86133 <eee>
2019-11-26 20:00:34 +0800

    lianxi

--- src/main/java/com/atguigu/crud/controller/DepartmentController.java
+++ src/main/java/com/atguigu/crud/controller/DepartmentController.java
@@ -11,24 +11,17 @@ import com.atguigu.crud.bean.Department;
 import com.atguigu.crud.bean.Msg;
 import com.atguigu.crud.service.DepartmentService;
 
-/**
- * 处理和部门有关的请求
- * @author lfy
- *
- */
+
 @Controller
 public class DepartmentController {
 	
 	@Autowired
 	private DepartmentService departmentService;
 	
-	/**
-	 * 返回所有的部门信息
-	 */
+
 	@RequestMapping("/depts")
 	@ResponseBody
 	public Msg getDepts(){
-		//查出的所有部门信息
 		List<Department> list = departmentService.getDepts();
 		return Msg.success().add("depts", list);
 	}
